21181870055fba06f34f4952c47b2140b0addafb
EL FELLAH Meryem <elfellahmeryem2@gmail.com>
2025-10-15 08:55:07 +0100

    creation des packages et interface IDao et son implementation dans la classe DaoImpl avec un code

---
+++ src/main/java/dao/DaoImpl.java
@@ -0,0 +1,11 @@
+package dao;
+
+public class DaoImpl implements IDao {
+
+    @Override
+    public double getData() {
+        System.out.println("version base de donnees");
+        double data = 34;
+        return data;
+    }
+}
